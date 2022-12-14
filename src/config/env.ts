c44ba3e0317a86191488d5d27021d937917e57b6
skynet118 <nico_8830_3@hotmail.com>
2022-12-14 11:52:13 -0500

    changes on endpoint

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,6 +1,5 @@
 
 export default {
     //"ELECTRIPURE_ENDPOINT": "https://flaskapi.store",//"http://flaskapi-env.eba-swfkr2ub.us-east-1.elasticbeanstalk.com"
-    //"ELECTRIPURE_ENDPOINT": "https://apidev.electripuremeter.com"
-    "ELECTRIPURE_ENDPOINT": "http://localhost:5000"
+    "ELECTRIPURE_ENDPOINT": "https://apidev.electripuremeter.com"
 };
